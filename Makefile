71c5e529f2857081b0731e7205703df55692757f
Armand Fardeau <armandfardeau@users.noreply.github.com>
2021-12-02 14:43:39 +0100

    Add basic auth token (#2)

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@ VERSION := latest
 REGISTRY_ENDPOINT := rg.$(REGION).scw.cloud
 REGISTRY_NAMESPACE := osp-internal-tools
 REGISTRY_TAG := $(REGISTRY_ENDPOINT)/$(REGISTRY_NAMESPACE)/$(IMAGE_NAME):$(VERSION)
+AUTH_TOKEN := dummy
 
 login:
 	docker login $(REGISTRY_ENDPOINT) -u userdoesnotmatter -p $(TOKEN)
@@ -21,7 +22,7 @@ build:
 	docker build -t $(IMAGE_NAME) . --compress --tag $(REGISTRY_TAG)
 
 run:
-	docker run -it -e PORT=$(PORT) -p $(PORT):$(PORT) --rm $(TAG)
+	docker run -it -e AUTH_TOKEN=$(AUTH_TOKEN) -e PORT=$(PORT) -p $(PORT):$(PORT) --rm $(REGISTRY_TAG)
 
 start:
 	@make build
--- app.py
+++ app.py
@@ -1,33 +1,38 @@
 # Import libraries
 from flask import Flask, request, jsonify
 from utils import cleaned_df
+from utils import check_for_token
 import pickle
 import json
 import pandas as pd
+
 app = Flask(__name__)
 # Load the model
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/api', methods=['POST'])
 def predict():
-    # Get the data from the POST request.
-    input = request.get_json(force=True)
-    model_data = pd.DataFrame(input)[[
-        'sign_in_count',
-        'personal_url',
-        'about',
-        'avatar',
-        'extended_data',
-        'followers_count',
-        'following_count',
-        'invitations_count',
-        'failed_attempts',
-        'admin']]
-    cleaned_data = cleaned_df(model_data)
-    # Make prediction using model loaded from disk as per the data.
-    prediction = model.predict_proba(cleaned_data)[:, 1]
-    for index,element in enumerate(input):
-        element['spam_probability'] = round(float(prediction[index]),4)
-    return jsonify(input)
+    if check_for_token(request) == False:
+        return "Unauthorized", 401
+    else:
+        # Get the data from the POST request.
+        input = request.get_json(force=True)
+        model_data = pd.DataFrame(input)[[
+            'sign_in_count',
+            'personal_url',
+            'about',
+            'avatar',
+            'extended_data',
+            'followers_count',
+            'following_count',
+            'invitations_count',
+            'failed_attempts',
+            'admin']]
+        cleaned_data = cleaned_df(model_data)
+        # Make prediction using model loaded from disk as per the data.
+        prediction = model.predict_proba(cleaned_data)[:, 1]
+        for index,element in enumerate(input):
+            element['spam_probability'] = round(float(prediction[index]),4)
+        return jsonify(input)
 if __name__ == '__main__':
     app.run()
